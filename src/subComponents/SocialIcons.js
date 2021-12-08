029c991a06651096a96802b5462d67fd87e7273a
djanoer <djanur405@gmail.com>
2021-12-08 14:52:19 +0800

    page intro

---
+++ src/subComponents/SocialIcons.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { Facebook, Github, Twitter, YouTube } from "../components/AllSvgs";
+import { DarkTheme } from "../components/Themes";
+
+const Icons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: fixed;
+  bottom: 0;
+  left: 2rem;
+
+  z-index: 3;
+
+  & > *:not(:last-child) {
+    margin: 0.3rem 0;
+  }
+`;
+
+const Line = styled.span`
+  width: 2px;
+  height: 6rem;
+  background-color: ${(props) => (props.color === "dark" ? DarkTheme.text : DarkTheme.body)};
+`;
+
+const SocialIcons = (props) => {
+  return (
+    <Icons>
+      <div>
+        <NavLink style={{ color: "inherit" }} target="_blank" to={{ pathname: "https://github.com/djanoer" }}>
+          <Github width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body} />
+        </NavLink>
+      </div>
+      <div>
+        <NavLink style={{ color: "inherit" }} target="_blank" to={{ pathname: "https://twitter.com/daveee_id" }}>
+          <Twitter width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body} />
+        </NavLink>
+      </div>
+      <div>
+        <NavLink style={{ color: "inherit" }} target="_blank" to={{ pathname: "https://facebook.com" }}>
+          <Facebook width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body} />
+        </NavLink>
+      </div>
+      <div>
+        <NavLink style={{ color: "inherit" }} target="_blank" to={{ pathname: "https://youtube.com" }}>
+          <YouTube width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body} />
+        </NavLink>
+      </div>
+
+      <Line color={props.theme} />
+    </Icons>
+  );
+};
+
+export default SocialIcons;
