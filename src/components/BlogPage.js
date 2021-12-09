16ab7485cc587b2a2ba312d1cab0b6d81f35b4de
djanoer <djanur405@gmail.com>
2021-12-09 16:19:13 +0800

    update blog page

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,9 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import img from "../assets/Images/robert-anasch-McX3XuJRsUM-unsplash.jpg";
+import img from "../assets/Images/dariusz-sankowski-3OiYMgDKJ6k-unsplash.jpg";
 import LogoComponent from "../subComponents/LogoComponent";
 import PowerButton from "../subComponents/PowerButton";
 import SocialIcons from "../subComponents/SocialIcons";
+
+import { Blogs } from "../data/BlogData";
+import BlogComponent from "./BlogComponent";
+import AnchorComponent from "../subComponents/Anchor";
 
 const MainContainer = styled.div`
   background-image: url(${img});
@@ -11,15 +15,29 @@ const MainContainer = styled.div`
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-position: center;
-  width: 100vw;
+  /* width: 100vw; */
 `;
 
 const Container = styled.div`
   background-color: ${(props) => `rgba(${props.theme.bodyRgba}, 0.8)`};
   width: 100%;
-  height: 100vh;
+  height: auto;
+
   position: relative;
   padding-top: 5rem;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+  grid-gap: calc(1rem + 2vw);
 `;
 
 const BlogPage = () => {
@@ -29,6 +47,14 @@ const BlogPage = () => {
         <LogoComponent />
         <PowerButton />
         <SocialIcons />
+        <AnchorComponent />
+        <Center>
+          <Grid>
+            {Blogs.map((blog) => {
+              return <BlogComponent key={blog.id} blog={blog} />;
+            })}
+          </Grid>
+        </Center>
       </Container>
     </MainContainer>
   );
