bcd7ce90f44678149fafd6dc85c435065af11a7a
djanoer <djanur405@gmail.com>
2021-12-10 11:18:55 +0800

    update myskillPage

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,30 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import img from "../assets/Images/dariusz-sankowski-3OiYMgDKJ6k-unsplash.jpg";
+import img from "../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg";
 import LogoComponent from "../subComponents/LogoComponent";
-import PowerButton from "../subComponents/PowerButton";
 import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
 
 import { Blogs } from "../data/BlogData";
 import BlogComponent from "./BlogComponent";
 import AnchorComponent from "../subComponents/Anchor";
+// import BigTitle from "../subComponents/BigTitle";
+import { motion } from "framer-motion";
 
-const MainContainer = styled.div`
+const MainContainer = styled(motion.div)`
   background-image: url(${img});
   background-size: cover;
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-position: center;
-  /* width: 100vw; */
 `;
-
 const Container = styled.div`
-  background-color: ${(props) => `rgba(${props.theme.bodyRgba}, 0.8)`};
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
   width: 100%;
   height: auto;
-
   position: relative;
-  padding-top: 5rem;
+  padding-bottom: 5rem;
 `;
 
 const Center = styled.div`
@@ -40,14 +39,42 @@ const Grid = styled.div`
   grid-gap: calc(1rem + 2vw);
 `;
 
+// Framer-motion config
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+
+    transition: {
+      staggerChildren: 0.5,
+      duration: 0.5,
+    },
+  },
+};
+
 const BlogPage = () => {
+  const [numbers, setNumbers] = useState(0);
+
+  useEffect(() => {
+    let num = (window.innerHeight - 70) / 30;
+    setNumbers(parseInt(num));
+  }, []);
+
   return (
-    <MainContainer>
+    <MainContainer
+      variants={container}
+      initial="hidden"
+      animate="show"
+      exit={{
+        opacity: 0,
+        transition: { duration: 0.5 },
+      }}
+    >
       <Container>
         <LogoComponent />
         <PowerButton />
         <SocialIcons />
-        <AnchorComponent />
+        <AnchorComponent number={numbers} />
         <Center>
           <Grid>
             {Blogs.map((blog) => {
@@ -55,6 +82,7 @@ const BlogPage = () => {
             })}
           </Grid>
         </Center>
+        {/* <BigTitle text="BLOG" top="5rem" left="5rem" /> */}
       </Container>
     </MainContainer>
   );
--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,7 +1,55 @@
 import React from "react";
+import { lightTheme } from "./Themes";
+import styled, { ThemeProvider } from "styled-components";
+import { Design, Develope } from "./AllSvgs";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Main = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+
+  font-family: "Ubuntu Mono", monospace;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Title = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const MySkillsPage = () => {
-  return <div>MySkillsPage Component/Page</div>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Box>
+        <Main>
+          <Title>
+            <Design width={40} height={40} /> Designer
+          </Title>
+        </Main>
+        <Main>
+          <Develope width={40} height={40} /> Frontend Developer
+        </Main>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default MySkillsPage;
